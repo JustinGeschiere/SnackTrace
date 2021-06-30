05fcefe8a3dc9a420f693b2797b492cdf7bea69b
Justin Geschiere <j.geschiere@hotmail.com>
2021-06-30 22:17:23 +0200

    Clean up

--- SnackTrace.GraphQL/Arguments/MenuArguments.cs
+++ SnackTrace.GraphQL/Arguments/MenuArguments.cs
@@ -1,7 +1,5 @@
-﻿using GraphQL.Types;
-using SnackTrace.GraphQL.Types.Order;
+﻿using SnackTrace.GraphQL.Types.Order;
 using SnackTrace.GraphQL.Types.Where;
-using System.Collections.Generic;
 
 namespace SnackTrace.GraphQL.Arguments
 {
--- SnackTrace.Services/Interfaces/IGraphService.cs
+++ SnackTrace.Services/Interfaces/IGraphService.cs
@@ -2,11 +2,8 @@
 
 namespace SnackTrace.Services.Interfaces
 {
-	public interface IGraphService<Tentity,Twhere,Torder> : IGraphService
+	public interface IGraphService<Tentity,Twhere,Torder> 
 	{
 		IEnumerable<Tentity> GetGraphEntities(Twhere where, Torder order, int skip, int take);
 	}
-
-	public interface IGraphService
-	{ }
 }
